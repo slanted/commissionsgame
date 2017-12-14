9d6704438d480317c6c4eac8e8b3656f04d755fb
Evan Larson <elarson@nuskin.com>
2017-12-14 14:23:06 -0700

    Add title page and increase game size

--- js/menu.js
+++ js/menu.js
@@ -6,14 +6,10 @@ var introText;
 Menu.prototype = {
   preload: function() {
 
-    this.game.load.image("playButton","assets/images/start_button.png");
+    this.game.load.image("titlepage","assets/images/titlepage.png");
   },
   create: function() {
-    introText = this.game.add.text(10, 10, 'Here is the intro to the game', {fontSize: '16px', fill: '#FFF'});
-    //var gameTitle = this.game.add.sprite(160,160,"title");
-    //gameTitle.anchor.setTo(0.5,0.5);
-    var playButton = this.game.add.button(160,320,"playButton",this.playTheGame,this);
-    playButton.anchor.setTo(0.5,.75);
+    var playButton = this.game.add.button(0,0,"titlepage",this.playTheGame,this);
   },
   playTheGame: function() {
     this.game.state.start("Level");
